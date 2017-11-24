509fd682c226498031977a66d74fdfbe68e62da1
balfaroc <bralfaroc@gmail.com>
2017-11-24 16:24:00 -0600

    tutorial 4 done
    -adding more views
    -adding more url
    -adding views for target model
    -adding update IP for target (dummy)

--- ValidationSite/polls/models.py
+++ ValidationSite/polls/models.py
@@ -5,17 +5,21 @@ from django.utils.encoding import python_2_unicode_compatible
 
 
 @python_2_unicode_compatible  # only if you need to support Python 2
+
 class Target(models.Model):
-    target_text = models.CharField(max_length=200)
+    name = models.CharField(max_length=50,default='NoName')
     IP = models.CharField(max_length=16,default='0.0.0.0')
     available = models.BooleanField(default=False)
+    status = models.CharField(max_length=16,default='NotConnected')
+    IPSS_version = models.CharField(max_length=50,default='0.0')
+    OS = models.CharField(max_length=50,default='0.0')
+    bios = models.CharField(max_length=50,default='0.0')
+    platform = models.CharField(max_length=50,default='NoPlatform')
     pub_date = models.DateTimeField('date published')
     def __str__(self):
-        return self.target_text
+        return self.name
     def was_published_recently(self):
         return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
-
-
 
 
 
--- ValidationSite/polls/views.py
+++ ValidationSite/polls/views.py
@@ -1,10 +1,11 @@
-from django.http import HttpResponse
-from django.template import loader
 from django.shortcuts import get_object_or_404, render
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.views import generic
 
 
-from .models import Question
-from .models import Target
+
+from .models import Question, Target, Choice
 
 
 #def index(request):
@@ -14,19 +15,64 @@ from .models import Target
 
 
 
-def index(request):
-    latest_question_list = Question.objects.order_by('-pub_date')[:5]
-    context = {'latest_question_list': latest_question_list}
-    return render(request, 'polls/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'polls/index.html'
+    context_object_name = 'latest_question_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Question.objects.order_by('-pub_date')[:5]
 
 
-def detail(request, question_id):
-    question = get_object_or_404(Question, pk=question_id)
-    return render(request, 'polls/detail.html', {'question': question})
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = 'polls/detail.html'
 
-def results(request, question_id):
-    response = "You're looking at the results of question %s."
-    return HttpResponse(response % question_id)
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = 'polls/results.html'
+
+
 
 def vote(request, question_id):
-    return HttpResponse("You're voting on question %s." % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST['choice'])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(request, 'polls/detail.html', {
+            'question': question,
+            'error_message': "You didn't select a choice.",
+        })
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse('polls:results', args=(question.id,)))
+
+
+class TargetIndexView(generic.ListView):
+    template_name = 'polls/targetindex.html'
+    context_object_name = 'latest_target_list'
+
+    def get_queryset(self):
+        """Return the last five published questions."""
+        return Target.objects.order_by('-pub_date')[:5]
+
+
+class TargetDetailView(generic.DetailView):
+    model = Target
+    template_name = 'polls/targetdetail.html'
+
+def update_IP(request, target_id):
+    target = get_object_or_404(Target, pk=target_id)
+
+    target.IP = 'hola mundo'
+    target.save()
+    # Always return an HttpResponseRedirect after successfully dealing
+    # with POST data. This prevents data from being posted twice if a
+    # user hits the Back button.
+    return HttpResponseRedirect(reverse('polls:targetdetail', args=(target.id,)))
